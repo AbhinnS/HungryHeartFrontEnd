49c3008be5890563423dc08c7123b8d8671bbb93
Abhinn Sayansh <sayanshabhinn@gmail.com>
2026-07-04 00:55:50 +0530

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5001/api",
-  baseURL: "https://hungryheart.onrender.com/",
+  baseURL: "https://hungryheart.onrender.com/api",
   
   headers: { "Content-Type": "application/json" },
 });
